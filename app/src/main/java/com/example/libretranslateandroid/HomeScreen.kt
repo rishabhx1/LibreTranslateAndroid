b2c01987573bf6379b4262359155aebcfce84ed2
Rishabh Jain <rishabhj10166@gmail.com>
2022-11-19 20:38:21 +0530

    Added Language selector and fixed the textfields.

--- app/src/main/java/com/example/libretranslateandroid/HomeScreen.kt
+++ app/src/main/java/com/example/libretranslateandroid/HomeScreen.kt
@@ -1,25 +1,26 @@
 package com.example.libretranslateandroid
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldColors
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.toSize
 import com.plcoding.meditationuiyoutube.ui.theme.Gray800
 import com.plcoding.meditationuiyoutube.ui.theme.Gray900
 
@@ -32,25 +33,32 @@ fun HomeScreen() {
         Column {
             Text(
                 text = "Libre Translate",
-                fontSize = 45.sp,
+                fontSize = 40.sp,
                 color = Color.White,
-                fontFamily = FontFamily.Serif,
+                fontFamily = FontFamily(
+                    Font(R.font.quicksand_bold)
+                ),
                 modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
             )
-            TextBox()
-            TextBox()
+            dropDownMenu()
+            translateBox()
+            dropDownMenu()
+            translatedBox()
         }
     }
 }
 
 @Composable
-fun TextBox() {
+fun translateBox() {
     Column {
         var text by remember { mutableStateOf("") }
         TextField(
             value = text,
             onValueChange = {newText ->
             text = newText },
+            placeholder = { Text(
+                text = "Type to translate",
+                fontSize = 25.sp) },
             maxLines = 10,
             textStyle = TextStyle(color = Color.White, fontSize = 25.sp),
             modifier = Modifier
@@ -60,8 +68,97 @@ fun TextBox() {
             shape = RoundedCornerShape(20.dp),
             colors = TextFieldDefaults.textFieldColors(
                 backgroundColor = Gray800,
-            )
-
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
         )
+    }
+}
+
+@Composable
+fun translatedBox() {
+    Column {
+        var text by remember { mutableStateOf("") }
+        TextField(
+            value = text,
+            onValueChange = {newText ->
+                text = newText },
+            maxLines = 10,
+            placeholder = { Text(
+                text = "Translation",
+                fontSize = 25.sp) },
+            textStyle = TextStyle(color = Color.White, fontSize = 25.sp),
+            modifier = Modifier
+                .padding(start = 8.dp, top = 12.dp, end = 8.dp, bottom = 12.dp)
+                .fillMaxWidth()
+                .height(200.dp),
+            shape = RoundedCornerShape(20.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Gray800,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            readOnly = true,
+        )
+    }
+}
+
+@Composable
+fun dropDownMenu() {
+
+    var expanded by remember { mutableStateOf(false) }
+    val list = listOf("English", "Spanish", "Japanese")
+    var selectedItem by remember { mutableStateOf("") }
+
+    var textFiledSize by remember { mutableStateOf(Size.Zero) }
+
+    val icon = if(expanded) {
+        Icons.Filled.KeyboardArrowUp
+    } else {
+        Icons.Filled.KeyboardArrowDown
+    }
+
+    Column(modifier = Modifier.padding(8.dp)) {
+        
+        OutlinedTextField(
+            value = selectedItem,
+            onValueChange = { selectedItem = it},
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    textFiledSize = coordinates.size.toSize()
+                },
+            label = { Text(
+                text = "Select Language",
+                color = Color.White,
+            ) },
+            trailingIcon = {
+                Icon(icon, "", Modifier.clickable { expanded = !expanded })
+            },
+            textStyle = TextStyle(color = Color.White),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Gray800,
+                unfocusedBorderColor = Gray800,
+            )
+        )
+        
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current){textFiledSize.width.toDp()})
+        ) {
+            list.forEach { label ->
+                DropdownMenuItem(onClick = {
+                    selectedItem = label
+                    expanded = false
+                }) {
+                    Text(text = label)
+                }
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/libretranslateandroid/ui/theme/Type.kt
+++ app/src/main/java/com/example/libretranslateandroid/ui/theme/Type.kt
@@ -2,9 +2,16 @@ package com.example.libretranslateandroid.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.libretranslateandroid.R
+
+private val Lexend = FontFamily(
+    Font(R.font.lexend_light, FontWeight.W300),
+    Font(R.font.lexend_regular, FontWeight.W400),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
